c93a314c052abb677bdb98710c9ef2572102486d
elipinska <ewa.lipinska@hotmail.com>
2018-06-04 11:30:50 +0100

    Get the first pass time to display

--- client/src/views/pass_times_view.js
+++ client/src/views/pass_times_view.js
@@ -4,12 +4,12 @@ const ISSData = require('../models/iss_data.js')
 const PassTimesView = function(container) {
   this.container = container;
   this.city = null;
+  this.issResponse = null;
 }
 
 PassTimesView.prototype.bindEvents = function () {
   PubSub.subscribe('CityView:city-selected', (evt) => {
     this.city = evt.detail;
-    this.render();
 
     const lng = this.city.lng;
     const lat = this.city.lat;
@@ -20,6 +20,9 @@ PassTimesView.prototype.bindEvents = function () {
 
   PubSub.subscribe('ISSData:data-ready', (evt) => {
     console.log(evt.detail);
+    this.issResponse = evt.detail;
+    this.render();
+
   });
 };
 
@@ -27,6 +30,13 @@ PassTimesView.prototype.render = function() {
   this.clearContainer();
   this.createHTMLElement('h2', 'city-name', this.city.name);
   this.createHTMLElement('p', 'country', this.city.fullCountryName);
+
+  const time = new Date(this.issResponse.response[0].risetime*1000);
+
+  this.createHTMLElement('p', 'iss-pass-time-1', time);
+
+
+  console.log(time);
 };
 
 PassTimesView.prototype.createHTMLElement = function(type, id, text) {
