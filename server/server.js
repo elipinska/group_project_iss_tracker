7c244243f786329a43f07a09437866cb026828e8
elipinska <ewa.lipinska@hotmail.com>
2018-06-07 01:41:48 +0100

    Configue app for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.static(publicPath));
 
 app.use(parser.json());
 
-app.listen(5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log(`Listening on port ${ this.address().port }`);
 });
 
